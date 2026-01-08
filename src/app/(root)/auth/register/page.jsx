a55f26deec46137f690920a2a3ffe6003ac7142b
uneebnasir8080-tech <uneebnasir8080@gmail.com>
2026-01-08 18:34:29 +0500

    first commit

---
+++ src/app/(root)/auth/register/page.jsx
@@ -0,0 +1,19 @@
+import { Card, CardContent } from '@/components/ui/card'
+import React from 'react'
+
+const RegisterPage = () => {
+  return (
+    <div className='flex h-screen items-center bg-gray-100'>
+      <Card className=" flex w-170 mx-auto items-center">
+        <CardContent>
+          {/* bg-img  */}
+          <div className=''>
+
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
+
+export default RegisterPage
---
+++ src/app/(root)/user/patientData/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PatientData = () => {
+  return (
+    <div>PatientData</div>
+  )
+}
+
+export default PatientData
