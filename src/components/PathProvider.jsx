a529e44c0eb4506a067f3b983b19e54ba4037940
Mac <mac@192.168.1.22>
2026-03-01 02:59:57 +0500

    SSR

--- src/components/PathProvider.jsx
+++ src/components/PathProvider.jsx
@@ -5,7 +5,6 @@ import { usePathname } from "next/navigation";
 
 export default function PathProvider({ children }) {
   const pathname = usePathname();
-  console.log('first', pathname)
    const pageName = pathname.split("/").filter(Boolean).pop() || "";
   return pageName;
 }
